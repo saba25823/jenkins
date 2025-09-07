2718d2b3225cf48306ef9941dd73fa3c7c728798
saba25823 <sababebo21@gmail.com>
2025-09-07 14:32:10 +0530

    samplename.java

--- samplename.java
+++ samplename.java
@@ -1,6 +1,6 @@
 public class samplename{
     public static void main(String[] args){
         String name ="samplename";
-        System.out.println("hello,my name is " + name); 
+        System.out.println("hello,my name is " + name);  
     }
 }
